cc0dfe6d291ea744d94b4c64863725818c86f653
파크종찬 <ziastar@naver.com>
2024-09-05 15:38:13 +0900

    class

---
+++ src/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = (props) => {
+    return(
+        <div>
+            <h3>상품 페이지입니다.(Product.js)</h3>
+        </div>
+    );
+}
+
+export default Product;
